7bf09710463d317c1aacaa6be2e16c96624ce4bf
shaongitt <shaongit@gmail.com>
2024-04-16 16:17:30 -0400

    login feature added with jquery

--- actions.php
+++ actions.php
@@ -1,11 +1,80 @@
 <?php
 
+require_once "db.php";
 
+
+
+function login_checker($conn){
+    
+
+    if(!isset($_REQUEST["email"])){
+     
+        $json = json_encode(array(
+            "status" => "error",
+            "msg" => "Sorry, Please provide an email address.",
+        ));
+        return $json;
+     
+    }
+    if(!isset($_REQUEST["password"])){
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, please enter a password. ",
+        ));
+
+        return $json;
+        
+
+    }
+
+    $email =  mysqli_real_escape_string( $conn,$_REQUEST["email"]);
+    $password  =  mysqli_real_escape_string( $conn,$_REQUEST["password"]);
+    $password = md5($password);
+
+
+
+    $sql = $conn->prepare("SELECT * FROM `users` WHERE `email` = ? and `password` = ?");
+    $sql->bind_param('ss',$email, $password);
+    $sql->execute();
+    $res = $sql->get_result();
+    
+
+    if ($res->num_rows > 0){
+
+        $json =  json_encode(array(
+            "status"=>"success",
+            "msg"=>"Logged in successfully",
+        ));
+        return $json;
+        
+
+    }
+    else{
+        $json = json_encode(array(
+        "status"=>"error",
+        "msg"=>"Sorry email and password combination does not match"
+        ));
+        return $json;
+        
+    }
+
+
+}
+
+
+
+  
 if ( isset($_GET["action"]) && isset($_POST)){
+  
 
-    $email = mysqli_escape_string($conn,) 
+    if($_GET["action"]=="login"){
+        
+        $json_out = login_checker($conn);
+        echo $json_out;
+    }
 
-    echo json_encode(['status'=>'ok']);
+
+    
 
 }
 
--- db.php
+++ db.php
@@ -4,6 +4,11 @@
        $password = "";
        $database ="ecom";
        $conn = mysqli_connect($servername,$username,$password,$database);
+
+       if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+      }
        
 ;?>
       
