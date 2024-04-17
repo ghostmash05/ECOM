846ca1c912503e150366dac29643c853cdc91d1d
shaongitt <shaongit@gmail.com>
2024-04-17 04:14:05 -0400

    registration form added

--- actions.php
+++ actions.php
@@ -27,7 +27,7 @@ function login_checker($conn){
 
     }
 
-    $email =  mysqli_real_escape_string( $conn,$_REQUEST["email"]);
+    $email =  mysqli_real_escape_string( $conn,$_POST["email"]);
     $password  =  mysqli_real_escape_string( $conn,$_REQUEST["password"]);
     $password = md5($password);
 
@@ -61,6 +61,174 @@ function login_checker($conn){
 
 }
 
+function register($conn){
+    if(!isset($_REQUEST["full_name"])){
+     
+        $json = json_encode(array(
+            "status" => "error",
+            "msg" => "Sorry, Please provide your full name.",
+        ));
+        return $json;
+     
+    }
+
+    if(!isset($_POST["email"])){
+     
+        $json = json_encode(array(
+            "status" => "error",
+            "msg" => "Sorry, Please provide an email address.",
+        ));
+        return $json;
+     
+    }
+    if(!isset($_POST["username"])){
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, please enter a username. ",
+        ));
+
+        return $json;
+        
+
+    }
+
+    if(!isset($_REQUEST["password"])){
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, please enter a password. ",
+        ));
+
+        return $json;
+        
+
+    }
+
+    
+
+    if(!isset($_REQUEST["cnf_password"])){
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, please enter password in confirm password field to confirm . ",
+        ));
+
+        return $json;
+        
+
+    }
+
+    if(!isset($_REQUEST["phone"])){
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, please provide a phone number. ",
+        ));
+
+        return $json;
+        
+
+    }
+
+    if($_REQUEST["password"]!=$_REQUEST["cnf_password"]){
+
+        $json= json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry, cofirm password don't match with the given password. ",
+        ));
+
+        return $json;
+    }
+    $full_name =  mysqli_real_escape_string($conn, $_REQUEST["full_name"]) ;
+    $email =  mysqli_real_escape_string($conn, $_POST["email"]) ;
+    $username =  mysqli_real_escape_string($conn, $_POST["username"]); 
+    $password =  mysqli_real_escape_string($conn, $_REQUEST["password"]); 
+    $password = md5($password);
+    $phone = mysqli_real_escape_string($conn, $_REQUEST["phone"]);
+
+
+
+    $sql_email_check = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $sql_email_check->bind_param("s", $email);
+    $sql_email_check->execute();
+    $email_check_res = $sql_email_check->get_result();
+
+    if($email_check_res->num_rows>0){
+      
+        $json = json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry an acccount with the email already exits",
+        ));
+
+        return $json;
+
+    }
+
+    $sql_username_check = $conn->prepare("SELECT * from users WHERE username = ?");
+    $sql_username_check->bind_param("s", $phone);
+    $sql_username_check->execute();
+    $username_check_res = $sql_username_check->get_result();
+
+    if($username_check_res->num_rows>0){
+
+        $json = json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry an acccount with the username already exits",
+        ));
+
+        return $json;
+
+    }
+
+    $sql_phone_check = $conn->prepare("SELECT * from users WHERE phone = ?");
+    $sql_phone_check->bind_param("s", $phone);
+    $sql_phone_check->execute();
+    $phone_check_res = $sql_phone_check->get_result();
+
+    if($phone_check_res->num_rows>0){
+      
+        $json = json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry an acccount with the phone number already exits",
+        ));
+
+        return $json;
+
+    }
+
+
+    $sql_account_create = $conn->prepare("INSERT INTO `users` (`full_name`, `username`, `email`, `password`, `phone`) VALUES (?, ?, ?, ?, ?); ");
+    $sql_account_create->bind_param("sssss",$full_name, $username, $email, $password, $phone);
+    $sql_account_create->execute();
+
+    
+
+    if ($sql_account_create){
+
+
+        $json = json_encode(array(
+            "status"=>"success",
+            "msg"=>"Thank you your account has been successfully created. ",
+        ));
+
+        return $json;
+
+    }
+    else{
+        $json = json_encode(array(
+            "status"=>"error",
+            "msg"=>"Sorry something went wrong. Please try again later. ",
+        ));
+
+        return $json;
+
+    }
+
+
+    
+
+
+
+
+}
+
 
 
   
@@ -70,6 +238,12 @@ if ( isset($_GET["action"]) && isset($_POST)){
     if($_GET["action"]=="login"){
         
         $json_out = login_checker($conn);
+        echo $json_out;
+    }
+
+    elseif($_GET["action"]=="register"){
+        
+        $json_out = register($conn);
         echo $json_out;
     }
 
