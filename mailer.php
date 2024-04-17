e71b1b450dbc5ef19fc2bf1436daa0e67e5ca423
shaongitt <shaongit@gmail.com>
2024-04-17 07:09:49 -0400

    added phpmailer

--- mailer.php
+++ mailer.php
@@ -1,20 +1,48 @@
+
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
 $smtp_server = "";
 $smtp_username = "";   
 $smtp_password = "";
-$smtp_port = ;
+$smtp_port = 42;
 
-$from = "admin@whoistheboss.com"
-$site_url = "http://localhost"
+$mail->Host       = $smtp_server;                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = smtp_username;                     //SMTP username
+$mail->Password   = smtp_password;                               //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+$mail->Port       = $smtp_port;  
+
+
+
+$from = "admin@whoistheboss.com";
+$site_url = "http://localhost";
+$site_name= "shoes ";
+
+$mail->setFrom('shaongit@gmail.com', $from);
 
 
 
 function password_reset_mail($email){
 
-$subject = "Password Reset";
+$mail->addAddress($email);
+$mail->isHTML(true); 
+$mail->Subject = "Password Reset - $sitename";
 $reset_link = "$site_url/password_reset.php";
-$msg = "Your password has been reset, Please follow <a href='$reset_link'> this link </a> to reset your password";
+$mail->Body = "Your password has been reset, Please follow <a href='$reset_link'> this link </a> to reset your password";
 
-
-
+$mail->send();
 
 }
+
+?>
+
+
